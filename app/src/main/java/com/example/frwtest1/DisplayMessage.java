9a1391566b499af792dbb95a62bd9c512e62600e
clarencewu94 <clarencewu94@users.noreply.github.com>
2019-08-17 15:37:35 -0700

    Finally fixed the Sync Issues, the project can now use the design tab, updated added some login but may redo them later, this version requires butterknife which is not yet implemented.
    Signed-off-by: clarencewu94 <clarencewu94@users.noreply.github.com>

--- app/src/main/java/com/example/frwtest1/DisplayMessage.java
+++ app/src/main/java/com/example/frwtest1/DisplayMessage.java
@@ -1,12 +1,10 @@
 package com.example.frwtest1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMessage extends AppCompatActivity {
 
--- app/src/main/java/com/example/frwtest1/MainActivity.java
+++ app/src/main/java/com/example/frwtest1/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.frwtest1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.FRWTest1.MESSAGE";
